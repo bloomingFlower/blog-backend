4458dbe63aa6eaaab38dd7fdc7502ae1c3274df7
AAII <sinsunby@gmail.com>
2024-01-07 16:43:43 +0900

    readme

--- README.md
+++ README.md
@@ -1,54 +1,15 @@
-## 쿠버네티스 구성 초기화
-1. kubeadm reset
-2. sudo apt-get purge kubeadm kubectl kubelet
-3. sudo rm -r /etc/kubernetes/
-4. sudo rm -r /var/lib/etcd/
-5. sudo rm -r /var/lib/kubelet
-6. sudo rm -r ~/.kube
-7. sudo systemctl daemon-reload
-8. sudo rm -rf /etc/cni/net.d
+## Kubernetes Cluster
+- CNI - Flannel
+- Load Balancer - Cloud Provider
+- Service Mesh - Istio
+- CD - ArgoCD
 
-## 쿠버네티스 설치
-sudo apt-get update
-apt-transport-https may be a dummy package; if so, you can skip that package
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-```shell
-    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-   ```
+## External Services
+- CI - Travis CI
 
-```shell
-    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-   ```
-```shell
-    sudo apt-get update
-    sudo apt-get install -y kubelet kubeadm kubectl
-    sudo apt-mark hold kubelet kubeadm kubectl
-   ```
+## Application
+- Backend - Golang (Fiber)
+- Frontend - ReactJS
 
-systemctl daemon-reload
-systemctl daemon-reload
-systemctl restart kubelet
-systemctl enable kubelet
-
-kubeadm init --pod-network-cidr=192.168.0.0/16 --control-plane-endpoint=129.154.213.18
-flannel --pod-network-cidr=10.244.0.0/16
-
-
-root@devops-worker-1:~# sudo iptables -A INPUT -p tcp --dport 8285 -j ACCEPT
-root@devops-worker-1:~# sudo iptables -A INPUT -p tcp --dport 8472 -j ACCEPT
-root@devops-worker-1:~# sudo iptables -A INPUT -p tcp --dport 30000:32767 -j ACCEPT
-root@devops-worker-1:~# sudo netfilter-persistent save
-
-root@instance-1:~# sudo iptables -A INPUT -p tcp --dport 8285 -j ACCEPT
-root@instance-1:~# sudo iptables -A INPUT -p tcp --dport 8472 -j ACCEPT
-root@instance-1:~# sudo iptables -A INPUT -p tcp --dport 30000:32767 -j ACCEPT
-root@devops-worker-1:~# sudo iptables -A INPUT -p udp --dport 8285 -j ACCEPT
-root@devops-worker-1:~# sudo iptables -A INPUT -p udp --dport 8472 -j ACCEPT
-
-kubectl create namespace argocd kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl get svc -n argocd
-
-containerd config default | tee /etc/containerd/config.toml
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml  
-service containerd restart
-service kubelet restart
+## TODO
+- [하] Vitess 전환 - https://sungchul-p.github.io/db-on-k8s-vitess-overview
--- controller/webThreeLoginController.go
+++ controller/webThreeLoginController.go
@@ -1,15 +1,9 @@
 package controller
 
 import (
-	"crypto/ecdsa"
 	"crypto/rand"
 	"encoding/hex"
-	"github.com/bloomingFlower/blog-backend/database"
-	"github.com/bloomingFlower/blog-backend/models"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gofiber/fiber/v2"
-	"math/big"
-	"strings"
 )
 
 type Session struct {
@@ -33,54 +27,67 @@ func MetamaskLogin(c *fiber.Ctx) error {
 	})
 }
 
-func MetamaskCallback(c *fiber.Ctx) error {
-	// Get the signed nonce and public address from the request
-	signedNonce := c.FormValue("signedNonce")
-	publicAddress := c.FormValue("publicAddress")
-
-	// Verify the signed nonce
-	sig := strings.TrimPrefix(signedNonce, "0x")
-	if len(sig) != 130 {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid signature")
-	}
-
-	sigBytes, err := hex.DecodeString(sig)
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid signature")
-	}
-
-	r, s := sigBytes[:32], sigBytes[32:64]
-	v := sigBytes[64]
-	if v < 27 {
-		v += 27
-	}
-
-	session := c.Locals("session").(*Session)
-	nonce, err := hex.DecodeString(session.Nonce)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString("Internal server error")
-	}
-
-	pubKey, err := crypto.SigToPub(crypto.Keccak256Hash(nonce).Bytes(), &ecdsa.Signature{R: new(big.Int).SetBytes(r), S: new(big.Int).SetBytes(s), V: v})	}
-	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
-	if !strings.EqualFold(recoveredAddr.Hex(), publicAddress) {
-		return c.Status(fiber.StatusUnauthorized).SendString("Unauthorized")
-	}
-
-	// The user is authenticated, create or update the user in the database
-	var user models.User
-	database.DB.Where("public_address = ?", publicAddress).First(&user)
-	if user.ID == 0 {
-		user = models.User{
-			PublicAddress: publicAddress,
-		}
-		database.DB.Create(&user)
-	} else {
-		database.DB.Save(&user)
-	}
-
-	return c.JSON(fiber.Map{
-		"user":    user,
-		"message": "User logged in successfully",
-	})
-}
+// TODO: metamask 로그인
+//func MetamaskCallback(c *fiber.Ctx) error {
+//	// Get the signed nonce and public address from the request
+//	signedNonce := c.FormValue("signedNonce")
+//	publicAddress := c.FormValue("publicAddress")
+//
+//	// Verify the signed nonce
+//	sig := strings.TrimPrefix(signedNonce, "0x")
+//	if len(sig) != 130 {
+//		return c.Status(fiber.StatusBadRequest).SendString("Invalid signature")
+//	}
+//
+//	sigBytes, err := hex.DecodeString(sig)
+//	if err != nil {
+//		return c.Status(fiber.StatusBadRequest).SendString("Invalid signature")
+//	}
+//
+//	r, s := sigBytes[:32], sigBytes[32:64]
+//	v := sigBytes[64]
+//	if v < 27 {
+//		v += 27
+//	}
+//
+//	session := c.Locals("session").(*Session)
+//	nonce, err := hex.DecodeString(session.Nonce)
+//	if err != nil {
+//		return c.Status(fiber.StatusInternalServerError).SendString("Internal server error")
+//	}
+//
+//	sig := make([]byte, 65)
+//	copy(sig[32-len(r):32], r)
+//	copy(sig[64-len(s):64], s)
+//	sig[64] = v
+//	if sig[64] < 27 {
+//		sig[64] += 27
+//	}
+//
+//	pubKey, err := crypto.SigToPub(crypto.Keccak256Hash(nonce).Bytes(), sig)
+//	if err != nil {
+//		return c.Status(fiber.StatusInternalServerError).SendString("Internal server error")
+//	}
+//	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
+//	if !strings.EqualFold(recoveredAddr.Hex(), publicAddress) {
+//		return c.Status(fiber.StatusUnauthorized).SendString("Unauthorized")
+//	}
+//	//
+//
+//	// The user is authenticated, create or update the user in the database
+//	var user models.User
+//	database.DB.Where("public_address = ?", publicAddress).First(&user)
+//	if user.ID == 0 {
+//		user = models.User{
+//			PublicAddress: publicAddress,
+//		}
+//		database.DB.Create(&user)
+//	} else {
+//		database.DB.Save(&user)
+//	}
+//
+//	return c.JSON(fiber.Map{
+//		"user":    user,
+//		"message": "User logged in successfully",
+//	})
+//}
