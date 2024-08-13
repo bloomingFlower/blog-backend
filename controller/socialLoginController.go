a796583e5e0e7010317f68bd2d68e9121d81d636
jy form air <sinsunby@gmail.com>
2024-08-13 17:49:46 +0900

    google login

--- controller/socialLoginController.go
+++ controller/socialLoginController.go
@@ -2,12 +2,13 @@ package controller
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"io"
 	"log"
-	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/bloomingFlower/blog-backend/database"
@@ -22,13 +23,9 @@ import (
 )
 
 var (
-	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_PD"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
-		Endpoint:     google.Endpoint,
-	}
+	googleOauthConfig *oauth2.Config
+	githubOauthConfig *oauth2.Config
+	frontendURL       string
 )
 
 func init() {
@@ -37,186 +34,170 @@ func init() {
 		log.Println("No .env file found")
 	}
 
-	// Use FRONTEND_URL for Google redirect if not set
-	redirectURL := os.Getenv("FRONTEND_URL")
-	if redirectURL == "" {
-		redirectURL = "http://localhost:8080"
+	// Set frontend URL
+	frontendURL = os.Getenv("FRONTEND_URL")
+	if frontendURL == "" {
+		frontendURL = "http://localhost:8080"
 	}
 
-	googleOauthConfig.RedirectURL = redirectURL + "/auth/google/callback"
-}
-
-func getGithubOauthConfig() *oauth2.Config {
-	redirectURL := os.Getenv("FRONTEND_URL")
-	if redirectURL == "" {
-		redirectURL = "http://localhost:8080"
+	// Initialize Google OAuth config
+	googleOauthConfig = &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_PD"),
+		RedirectURL:  frontendURL + "/auth/google/callback",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
 	}
 
-	return &oauth2.Config{
-		RedirectURL:  redirectURL + "/auth/github/callback",
+	// Initialize GitHub OAuth config
+	githubOauthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
 		ClientSecret: os.Getenv("GITHUB_CLIENT_PD"),
-		Scopes:       []string{"user", "user:email"},
+		RedirectURL:  frontendURL + "/auth/github/callback",
+		Scopes:       []string{"user:email"},
 		Endpoint:     github.Endpoint,
 	}
+
+	// Log OAuth configurations
+	log.Println("OAuth configurations initialized")
+	log.Printf("Google RedirectURL: %s\n", googleOauthConfig.RedirectURL)
+	log.Printf("GitHub RedirectURL: %s\n", githubOauthConfig.RedirectURL)
 }
 
-func GoogleLogin(c *fiber.Ctx) error {
-	log.Println("GoogleLogin")
-	url := googleOauthConfig.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	log.Println("GoogleLogin", url)
-	return c.Redirect(url)
+// generateStateOauthCookie generates a random state string and sets it in a cookie
+func generateStateOauthCookie(c *fiber.Ctx) (string, error) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	state := base64.URLEncoding.EncodeToString(b)
+
+	cookie := fiber.Cookie{
+		Name:     "oauthstate",
+		Value:    state,
+		Expires:  time.Now().Add(time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+
+	return state, nil
 }
 
-func GoogleCallback(c *fiber.Ctx) error {
-	log.Println("GoogleCallback")
-	code := c.Query("code")
-	token, err := googleOauthConfig.Exchange(context.Background(), code)
+// socialLogin is a generic function for handling social logins
+func socialLogin(c *fiber.Ctx, oauthConfig *oauth2.Config) error {
+	state, err := generateStateOauthCookie(c)
 	if err != nil {
-		return c.Status(http.StatusUnauthorized).SendString("Unauthorized")
+		log.Printf("Error generating state: %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Internal server error",
+		})
 	}
 
-	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
-	if err != nil {
-		return c.Status(http.StatusInternalServerError).SendString("Internal server error")
-	}
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			return
-		}
-	}(response.Body)
-	data, err := io.ReadAll(response.Body)
-	if err != nil {
-		return c.Status(http.StatusInternalServerError).SendString("Internal server error")
-	}
-	var userData map[string]interface{}
-	err = json.Unmarshal(data, &userData)
-	if err != nil {
-		return c.Status(http.StatusInternalServerError).SendString("Internal server error")
-	}
-
-	var user models.User
-	database.DB.Where("email = ?", userData["email"]).First(&user)
-	if user.ID == 0 {
-		user = models.User{
-			FirstName: userData["given_name"].(string),
-			LastName:  userData["family_name"].(string),
-			Email:     userData["email"].(string),
-			Picture:   userData["picture"].(string),
-		}
-		database.DB.Create(&user)
-	}
-
-	return c.JSON(fiber.Map{
-		"user":    user,
-		"message": "User logged in successfully",
-	})
-}
-
-func GithubLogin(c *fiber.Ctx) error {
-	githubOauthConfig := getGithubOauthConfig()
-	url := githubOauthConfig.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.SetAuthURLParam("scope", "user:email"))
+	url := oauthConfig.AuthCodeURL(state, oauth2.AccessTypeOffline)
 	return c.JSON(fiber.Map{
 		"url": url,
 	})
 }
 
-func GithubCallback(c *fiber.Ctx) error {
-	githubOauthConfig := getGithubOauthConfig()
+func GoogleLogin(c *fiber.Ctx) error {
+	return socialLogin(c, googleOauthConfig)
+}
+
+func GithubLogin(c *fiber.Ctx) error {
+	return socialLogin(c, githubOauthConfig)
+}
+
+// socialCallback is a generic function for handling social login callbacks
+func socialCallback(c *fiber.Ctx, oauthConfig *oauth2.Config, userInfoURL string, mapUserData func(map[string]interface{}) models.User) error {
+	// State validation
+	state := c.Query("state")
+	oauthState := c.Cookies("oauthstate")
+	if state != oauthState {
+		log.Printf("Invalid oauth state, expected %s, got %s", oauthState, state)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid OAuth state",
+		})
+	}
+
 	code := c.Query("code")
-	token, err := githubOauthConfig.Exchange(context.Background(), code)
+	token, err := oauthConfig.Exchange(context.Background(), code)
 	if err != nil {
-		log.Println("Token Exchange Error:", err)
-		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthorized",
+		log.Printf("Code exchange failed: %v", err)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Code exchange failed",
 		})
 	}
 
-	client := githubOauthConfig.Client(context.Background(), token)
-	resp, err := client.Get("https://api.github.com/user")
+	client := oauthConfig.Client(context.Background(), token)
+	response, err := client.Get(userInfoURL)
 	if err != nil {
-		log.Println("GitHub API Error:", err)
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Internal server error",
+		log.Printf("Failed getting user info: %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed getting user info",
 		})
 	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	data, err := io.ReadAll(response.Body)
 	if err != nil {
-		log.Println("Read Body Error:", err)
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Internal server error",
+		log.Printf("Failed reading response body: %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed reading response body",
 		})
 	}
 
 	var userData map[string]interface{}
-	err = json.Unmarshal(body, &userData)
+	err = json.Unmarshal(data, &userData)
 	if err != nil {
-		log.Println("JSON Unmarshal Error:", err)
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Internal server error",
+		log.Printf("Failed to parse user data: %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to parse user data",
 		})
 	}
 
-	githubID, ok := userData["id"].(float64)
-	if !ok {
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Invalid GitHub ID",
-		})
-	}
+	user := mapUserData(userData)
 
-	var user models.User
-	result := database.DB.Where("github_id = ?", int64(githubID)).First(&user)
+	// Create or update user
+	var existingUser models.User
+	result := database.DB.Where("email = ?", user.Email).First(&existingUser)
 	if result.Error != nil {
 		if result.Error == gorm.ErrRecordNotFound {
-			// New user creation
-			user = models.User{
-				GithubID: int64(githubID),
-			}
-
-			// Handle login field from GitHub
-			if login, ok := userData["login"].(string); ok {
-				user.FirstName = login
-			}
-
-			// Handle email field from GitHub
-			if email, ok := userData["email"].(string); ok {
-				user.Email = email
-			} else {
-				// Get email from GitHub API if not provided
-				email, err := getGithubUserEmail(client)
-				if err == nil {
-					user.Email = email
-				}
-			}
-
 			if err := database.DB.Create(&user).Error; err != nil {
-				log.Println("User Creation Error:", err)
-				return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-					"message": "Failed to create user",
+				log.Printf("Failed to create user: %v", err)
+				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+					"error": "Failed to create user",
 				})
 			}
 		} else {
-			log.Println("Database Error:", result.Error)
-			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-				"message": "Database error",
+			log.Printf("Database error: %v", result.Error)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Database error",
+			})
+		}
+	} else {
+		user.ID = existingUser.ID
+		if err := database.DB.Save(&user).Error; err != nil {
+			log.Printf("Failed to update user: %v", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Failed to update user",
 			})
 		}
 	}
 
-	// JWT token generation
+	// Generate JWT token
 	jwtToken, err := util.GenerateJwt(user.ID)
 	if err != nil {
-		log.Println("JWT Generation Error:", err)
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Unable to login",
+		log.Printf("Failed to generate JWT: %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to generate JWT",
 		})
 	}
 
-	// Set cookie
+	// Set JWT token as cookie
 	cookie := fiber.Cookie{
 		Name:     "jwt",
 		Value:    jwtToken,
@@ -232,29 +213,34 @@ func GithubCallback(c *fiber.Ctx) error {
 	})
 }
 
-// Function to get GitHub user email
-func getGithubUserEmail(client *http.Client) (string, error) {
-	resp, err := client.Get("https://api.github.com/user/emails")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	var emails []struct {
-		Email    string `json:"email"`
-		Primary  bool   `json:"primary"`
-		Verified bool   `json:"verified"`
-	}
-
-	if err := json.NewDecoder(resp.Body).Decode(&emails); err != nil {
-		return "", err
-	}
-
-	for _, email := range emails {
-		if email.Primary && email.Verified {
-			return email.Email, nil
+func GoogleCallback(c *fiber.Ctx) error {
+	return socialCallback(c, googleOauthConfig, "https://www.googleapis.com/oauth2/v2/userinfo", func(userData map[string]interface{}) models.User {
+		return models.User{
+			FirstName: userData["given_name"].(string),
+			LastName:  userData["family_name"].(string),
+			Email:     userData["email"].(string),
+			Picture:   userData["picture"].(string),
 		}
-	}
+	})
+}
 
-	return "", errors.New("no primary verified email found")
+func GithubCallback(c *fiber.Ctx) error {
+	return socialCallback(c, githubOauthConfig, "https://api.github.com/user", func(userData map[string]interface{}) models.User {
+		user := models.User{
+			Email: userData["email"].(string),
+		}
+		if name, ok := userData["name"].(string); ok {
+			names := strings.Split(name, " ")
+			if len(names) > 0 {
+				user.FirstName = names[0]
+			}
+			if len(names) > 1 {
+				user.LastName = strings.Join(names[1:], " ")
+			}
+		}
+		if avatar, ok := userData["avatar_url"].(string); ok {
+			user.Picture = avatar
+		}
+		return user
+	})
 }
